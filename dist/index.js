3f8227f02101b43d537cf49b50241df45cf6184b
zhoutk <zhoutk@189.cn>
2018-02-08 18:26:45 +0800

    test debug.

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+class person {
+    constructor() {
+    }
+    print() {
+        console.log('this is a person');
+    }
+}
+let p = new person();
+p.print();
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+class person {
+    constructor() {
+
+    }
+
+    public print() {
+        console.log('this is a person')
+    }
+}
+let p = new person();
+p.print();
